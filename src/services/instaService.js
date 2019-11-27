a44dc6314d2df1eecd55087991e07bdc9ef5765d
Dm[I]tR1y <57756096+dimitriy3@users.noreply.github.com>
2019-11-27 19:47:44 +0700

    Add files via upload

---
+++ src/services/instaService.js
@@ -0,0 +1,30 @@
+export default class InstaService{
+    constructor(){
+        this._apiBase = 'http://localhost:3000';
+    }
+    getResource = async(url)=>{
+        const res =await fetch(`${this._apiBase}${url}`);
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}; recived ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllPosts = async(url)=>{
+        const res = await this.getResource('/posts/');
+        return res;
+    }
+
+    getAllPhotos = async(url)=>{
+        const res = await this.getResource('/posts/');
+        return res.map(this._transformPosts);
+    }
+
+    _transformPosts = (post)=>{
+        return{
+            src:post.src,
+            alt:post.alt
+        }
+    }
+}
